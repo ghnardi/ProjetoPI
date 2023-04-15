42f71ccd53c309796c9e8289f3ef68a497814d02
Thiago <thiago.romaris@sptech.school>
2023-04-15 00:57:25 -0300

    @thigarcias Ajustando as Headers das Páginas e adicionando o menu flutuante nelas

---
+++ Site Instituicional/PáginaAjuda/script.js
@@ -0,0 +1,14 @@
+function clickPerfil() {
+    if (subsub.style.display == 'flex') {
+        subsub.style.display = 'none'
+    } else {
+        subsub.style.display = 'flex'
+    }
+
+
+}
+
+
+function dashboard() {
+    window.location.href = "../PaginadeConsulta/consulta.html"
+}
