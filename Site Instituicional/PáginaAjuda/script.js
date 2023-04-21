2f9eab5b73a3c4ec87cca6179975c6fcdcfef262
Douglas Queiroz <douglas.queiroz@sptech.school>
2023-04-21 18:34:34 -0300

    @DouglasSDQ Nova Funcionalidades adicionadas a pagina de Ajuda

--- Site Instituicional/PáginaAjuda/script.js
+++ Site Instituicional/PáginaAjuda/script.js
@@ -16,17 +16,20 @@ function dashboard() {
 }
 
 function gerenciarconta(){
-    window.location.href = '/PaginaGerenciarConta/gerenciar.html'
+    window.location.href = '../PaginaGerenciarConta/gerenciar.html'
 }
 
 
 tecnologiaBloco.style.backgroundColor = '#F0EBDF'
 financeiroBloco.style.backgroundColor = '#F0EBDF'
+parceriaBloco.style.backgroundColor = '#F0EBDF'
 
 
 function categoriaTecnologia() {
     financeiroBloco.style.backgroundColor = '#F0EBDF'
     financeiroBloco.style.color = '#493510'
+    parceriaBloco.style.backgroundColor = '#F0EBDF'
+    parceriaBloco.style.color = '#493510'
 
     if (tecnologiaBloco.style.backgroundColor == 'rgb(240, 235, 223)') {
         blocaoMarromInfo.style.display = 'flex'
@@ -34,7 +37,8 @@ function categoriaTecnologia() {
 
 //deixar selecionado
         categoriaExibida.innerHTML = 'Tecnologia'
-        fraseExibidaFinanceiro.style.display = 'flex'
+        fraseExibida.innerHTML = 'ESPECIFICAÇÃO DHT11<br><br> Dimensões: 23mm x 12mm x 5mm (incluindo terminais)<br>Alimentação: 3,0 a 5,0 VDC (5,5 Vdc máximo)<br>Corrente: 200uA a 500mA, em stand by de 100uA a 150 uA<br>Faixa de medição de umidade: 20 a 90% UR<br>Faixa de medição de temperatura: 0º a 50ºC<br>Precisão de umidade de medição: ± 5,0% UR<br>Precisão de medição de temperatura: ± 2.0 ºC<br>Tempo de resposta: < 5s<br>'
+        fraseExibida.style.display = 'flex'
         tecnologiaBloco.style.backgroundColor = '#493510'
         tecnologiaBloco.style.color = '#F0EBDF'
     }
@@ -43,15 +47,44 @@ function categoriaTecnologia() {
 function categoriaFinanceiro() {
     tecnologiaBloco.style.backgroundColor = '#F0EBDF'
     tecnologiaBloco.style.color = '#493510'
+    parceriaBloco.style.backgroundColor = '#F0EBDF'
+    parceriaBloco.style.color = '#493510'
+
 
     if (financeiroBloco.style.backgroundColor == 'rgb(240, 235, 223)') {
         blocaoMarromInfo.style.display = 'flex'
         blocaoMarrom.style.display = 'none'
+        window.location.href = '../PáginaSobre/pagina-sobre.html#aqui'
+        
 
 //deixar selecionado
-        categoriaExibida.innerHTML = 'financeiro'
-        fraseExibidaFinanceiro.style.display = 'flex'
+        categoriaExibida.innerHTML = 'Financeiro'
+        fraseExibida.style.display = 'flex'
         financeiroBloco.style.backgroundColor = '#493510'
         financeiroBloco.style.color = '#F0EBDF'
     }
+}
+
+function categoriaParceria() {
+    tecnologiaBloco.style.backgroundColor = '#F0EBDF'
+    tecnologiaBloco.style.color = '#493510'
+    financeiroBloco.style.backgroundColor = '#F0EBDF'
+    financeiroBloco.style.color = '#493510'
+
+    if (parceriaBloco.style.backgroundColor == 'rgb(240, 235, 223)') {
+        blocaoMarromInfo.style.display = 'flex'
+        blocaoMarrom.style.display = 'none'
+
+//deixar selecionado
+        categoriaExibida.innerHTML = 'Parceria'
+        fraseExibida.innerHTML = 'Gostaríamos de convidá-lo para uma parceria com a nossa empresa, a Arttech. Somos uma empresa que busca inovação e criatividade em todas as áreas de atuação, buscando sempre as melhores soluções para nossos clientes. Nós valorizamos muito a colaboração e estamos ansiosos para trabalhar com você, caso tenha interesse. Por favor, entre em contato conosco para discutirmos mais detalhes sobre essa parceria. Juntos, podemos construir algo realmente incrível!'
+        fraseExibida.style.display = 'flex'
+        parceriaBloco.style.backgroundColor = '#493510'
+        parceriaBloco.style.color = '#F0EBDF'
+    }
+}
+
+function categoriaCadastro() {
+        window.location.href = '../PáginaCadastro/cadastro.html'
+    
 }
